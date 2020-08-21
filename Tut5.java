859359f87c182ca5943235436fcc6a7f01bf7015
Elliot Parker <elliotccp@protonmail.com>
2020-08-21 12:13:42 +1000

    Add part 2 content

--- Tut5.java
+++ Tut5.java
@@ -1,6 +1,19 @@
+import java.util.*;
+
 public class Tut5 {
+
+    static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
         int[] data = new int[100];
         int input = 0;
+
+        for (int i = 0; i < 100 && input != -1; i++) {
+            input = scan.nextInt();
+            if (input == -1) {
+                break;
+            }
+            data[i] = input;
+        }
     }
 }

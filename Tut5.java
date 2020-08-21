e415b4a463159cd42e5ab2c6745b80fa688b2efc
Elliot Parker <elliotccp@protonmail.com>
2020-08-21 12:23:54 +1000

    Add task 4 content

--- Tut5.java
+++ Tut5.java
@@ -9,6 +9,7 @@ public class Tut5 {
         int input = 0;
         int i = 0;
 
+        System.out.println("Input numbers (Input 100 numbers or '-1' to end):");
         for (i = 0; i < 100; i++) {
             input = scan.nextInt();
             if (input == -1) {
@@ -17,8 +18,26 @@ public class Tut5 {
             data[i] = input;
         }
 
+        System.out.println("Numbers in Array:");
         for (int j = 0; j < i; j++) {
             System.out.print(data[j] + " ");
+        }
+
+        System.out.print("Input Number to Search: ");
+        input = scan.nextInt();
+
+        boolean found = false;
+        for (int j = 0; j < i; j++) {
+            if (data[j] == input) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found == true) {
+            System.out.println("Number In Array");
+        } else {
+            System.out.println("Number NOT In Array");
         }
     }
 }
